bcd56952141286b9742b82372a8f582865d5e3b4
aafricke <33527666+aafricke@users.noreply.github.com>
2021-10-12 16:10:58 -0400

    Added Range Search

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import csv
 import pandas as pd
 
 def getMake():
-    make = input("Enter desired car Make: (i.e. Tesla)")
+    make = input("Enter desired car Make: (i.e. TESLA)")
     return(make)
-    
 
+def getRange():
+    crange = input("Enter minimum desired hours the car can drive before depletion: (i.e. 5)")
+    return(crange)
 
 def createCSV():
     column_name = ["make", "range(hr)"]
@@ -14,18 +17,27 @@ def createCSV():
     df2 = pd.read_csv('range.csv')
 
     userMake = getMake()
+    userRange = getRange()
     
     i = 0
 
     dfMake = df1["Make"]
+    dfRange = df1["TIME (h)"]
+    
     for row in dfMake:
         
-        careMake = dfMake[i]
-        if careMake == userMake :
+        carMake = dfMake[i]
+        carRange = dfRange[i]
+        
+        if carMake == userMake and carRange == int(userRange):
             df3.loc[i] = dfMake.loc[i]
+            df3.loc[i,"range(hr)"] = dfRange.loc[i]
         i += 1
+        
 
     print("\n")
     print(df3)
-        
+
+
+createCSV()
 
